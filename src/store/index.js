a1c7f3a02ccb8b67456cd97c6880db149cc4ab84
Aniket Aryamane <aryamane.aniket@gmail.com>
2021-05-17 23:52:46 +0530

    Vuex Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,46 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
   state: {
+    counter: 0,
   },
   mutations: {
+    INCREASECOUNTER(state, payload) {
+      state.counter += payload;
+    },
+    DECREASECOUNTER(state, payload) {
+      if (state.counter - payload > 0) {
+        state.counter -= payload;
+      } else {
+        state.counter = 0;
+      }
+    },
   },
   actions: {
+    async increaseCounter({ commit }) {
+      await axios
+        .get(
+          "https://www.random.org/integers/?num=1&min=1&max=9&col=5&base=10&format=plain&rnd=new"
+        )
+        .then((response) => {
+          commit("INCREASECOUNTER", response.data);
+        });
+    },
+    async decreaseCounter({ commit }) {
+      await axios
+        .get(
+          "https://www.random.org/integers/?num=1&min=1&max=9&col=5&base=10&format=plain&rnd=new"
+        )
+        .then((response) => {
+          commit("DECREASECOUNTER", response.data);
+        });
+    },
   },
-  modules: {
-  }
-})
+  getters: {
+    counterSquared(state) {
+      return state.counter * state.counter;
+    },
+  },
+  modules: {},
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+  <div class="counter">{{ $store.state.counter }}</div>
+  <div class="counter-squared">
+    {{ $store.state.counter }}<sup>2</sup> = {{ $store.getters.counterSquared }}
+  </div>
+  <div class="buttons">
+    <button @click="$store.dispatch('decreaseCounter')">-</button>
+    <button @click="$store.dispatch('increaseCounter')">+</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style scoped>
+div {
+  margin-bottom: 10px;
+}
+.counter {
+  font-size: 80px;
+}
+.buttons button {
+  font-size: 40px;
+  margin: 0 10px;
+  width: 100px;
+}
+</style>
